0b9aef20b59018f0b40b51749cb1ef24458e07df
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-01-16 13:08:11 +0530

    birthday finished

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
 import './App.css';
+import List from './List';
+import data from './data';
 
 function App() {
+
+  const [people, setPeople] = useState(data);
+
   return (
     <div className="App">
       <section className='container'>
-        <h3> 0 birthdays today</h3>
+        <h3> {people.length} birthdays today</h3>
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </div>
   );
--- birthday-reminder/src/data.js
+++ birthday-reminder/src/data.js
@@ -1,4 +1,4 @@
-export default data[
+export default [
     {
       id: 1,
       name: 'Bertie Yates',
