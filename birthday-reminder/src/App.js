b0d79eb55a1464606ea49a13368fb3e452b7bc34
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-01-16 12:51:41 +0530

    birthday-reminder app

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className='container'>
+        <h3> 0 birthdays today</h3>
+      </section>
+    </div>
+  );
+}
+
+export default App;
