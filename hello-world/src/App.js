6ee4c0e2a405e0e5f1705e47c0500a73e4532bb6
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-01-31 22:02:35 +0530

    new lines

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,11 +3,12 @@ import Greet from './Greet';
 import Welcome from './Welcome';
 import Hello from './Hello';
 import Message from './Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-    <Message name="Mike" heroName="Mickey Mouse" />
+      <Counter />
     </div>
   );
 }
