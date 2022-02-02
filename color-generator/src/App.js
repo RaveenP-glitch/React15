7b44739fc6f928373e981e3c02a93aecfb540d08
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-02-02 16:08:40 +0530

    new lines

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Values from 'values.js';
 
 function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+
+  }
+
   return (
-    <div className="App">
-      <h2>color generator setup</h2>
-      
-    </div>
+    <>
+      <section className='container'>
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type = "text" value={color} onChange={(e) => setColor(e.target.value)}/>
+        <button className='btn' type='submit'>
+          submit
+        </button>
+      </form>
+
+      </section>
+
+
+      <section className='container'>
+      <h4>list goes here</h4>
+
+      </section>
+    </>
+
   );
 }
 
