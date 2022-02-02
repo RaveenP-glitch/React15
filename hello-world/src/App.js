205ecc9e1586803fb510a04b4c3e9eb2d2b01195
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-02-03 00:47:05 +0530

    Event binding

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,11 +4,19 @@ import Welcome from './Welcome';
 import Hello from './Hello';
 import Message from './Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <FunctionClick />
+      <ClassClick /> */}
+      {/* <EventBind /> */}
+      <ParentComponent />
+
     </div>
   );
 }
--- hello-world/src/components/Counter.js
+++ hello-world/src/components/Counter.js
@@ -11,10 +11,21 @@ class Counter extends Component {
     }
 
     increment(){
-        this.setState({
-            count: this.state.count+1
+        this.setState((prevState)=> ({
+          count: prevState.count+1
+        }),() => {
+          console.log('Callback value', this.state.count)
         })
         console.log(this.state.count);
+
+    }
+
+    incrementFive(){
+      this.increment()
+      this.increment()
+      this.increment()
+      this.increment()
+      this.increment()
 
     }
     
@@ -23,7 +34,7 @@ class Counter extends Component {
   render() {
     return <div>
         <div>Count - {this.state.count}</div>
-        <button onClick={() => this.increment()}>Increment</button>
+        <button onClick={() => this.incrementFive()}>Increment</button>
     </div>;
   }
 }
