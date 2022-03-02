79dee8434808214c295612e637ba64d30d2207e6
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-03-02 18:03:25 +0530

    new lines

---
+++ jsonpuller/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  const [post, setPost] = useState({})
+  const [id, setId] = useState(1)
+
+
+
+  return (
+    <div className="App">
+        axios
+        .get()
+        .then()
+        .catch()
+
+
+    </div>
+  );
+}
+
+export default App;
